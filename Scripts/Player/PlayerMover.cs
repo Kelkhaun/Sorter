99467ffaf10fe7a65f5e3eb4fd40dd52dcbd7fec
Ilya Shilov <80789057+Kelkhaun@users.noreply.github.com>
2022-08-11 20:43:36 +0300

    Update PlayerMover.cs

--- Scripts/Player/PlayerMover.cs
+++ Scripts/Player/PlayerMover.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMover : MonoBehaviour
 {
+    private const float ZeroSpeed = 0;
+
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _rotateSpeed;
 
@@ -22,15 +24,14 @@ public class PlayerMover : MonoBehaviour
     {
         float verticalMove = Input.GetAxisRaw("Vertical") * _movementSpeed * Time.fixedDeltaTime;
         float horizontalMove = Input.GetAxisRaw("Horizontal") * _rotateSpeed * Time.fixedDeltaTime;
-        float zeroSpeed = 0;
         Vector3 rotation = new Vector3(transform.rotation.x, horizontalMove, transform.rotation.z);
 
-        if (horizontalMove != zeroSpeed)
+        if (horizontalMove != ZeroSpeed)
             transform.Rotate(rotation);
         else
             _rigidbody.angularVelocity = Vector3.zero;
 
-        if (verticalMove < zeroSpeed)
+        if (verticalMove < ZeroSpeed)
             return;
 
         _rigidbody.MovePosition(transform.position + transform.forward * verticalMove);
